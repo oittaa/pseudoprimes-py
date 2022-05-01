c9cdecb79a7b21dff9aa438fe8ad7ba597600361
oittaa <8972248+oittaa@users.noreply.github.com>
2022-05-01 08:48:38 +0300

    Refactor Baillie–PSW primality test to own file (#12)

---
+++ src/pseudoprimes/baillie_psw.py
@@ -0,0 +1,22 @@
+"""
+https://en.wikipedia.org/wiki/Baillie-PSW_primality_test
+"""
+
+from . import lucas, miller_rabin
+
+
+def is_baillie_psw_prp(n: int) -> bool:
+    """
+    Baillie–PSW primality test.
+
+    Arguments:
+        {n} integer -- Number to test.
+    Returns:
+        True -- If {n} is (probably) a prime.
+        False -- If {n} is not a prime.
+    """
+    return miller_rabin.is_miller_rabin_prp(n, (2,)) and lucas.is_strong_lucas_prp(n)
+
+    # Add a random M-R base
+    # bases = (2, secrets.SystemRandom().randrange(3, n - 1))
+    # return miller_rabin.is_miller_rabin_prp(n, bases) and lucas.is_strong_lucas_prp(n)
